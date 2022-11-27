8f6c1e44e233c020eea0e822eb7f9635247cc34b
kikato <35584539+kikato2022@users.noreply.github.com>
2022-11-27 14:18:55 +0800

    Update 7-1.cpp

--- ch7/7-1.cpp
+++ ch7/7-1.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
 
+using namespace std;
+
+double average(double x, double y);
+
+int main() {
+	cout << "Please input two number:\n";
+	double x,y;
+	while(cin>>x>>y){
+		if(x==0||y==0) break;
+		cout << average(x,y) << endl;
+		cout << "Please input two number:\n";
+	}
+	cout << "End." << endl;
+
+}
+
+double average(double x, double y){
+	return 2.0*x*y/(x+y);
+}
